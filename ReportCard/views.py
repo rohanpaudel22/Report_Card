a074189b43f3036ea18ad4291a71eb9939fc0ca8
rohanpaudel22 <poudelrohan2060@gmail.com>
2025-02-28 23:36:28 +0545

    adding some student records

--- ReportCard/views.py
+++ ReportCard/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import *
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
 
-# Create your views here.
+def get_students(request):
+  queryset = Student.objects.all()
+  
+  if request.GET.get('search'):
+    search = request.GET.get('search')
+    queryset = queryset.filter(
+      Q(student_name__icontains = search ) |
+      Q(department__department__icontains = search ) |
+      Q(student_id__student_id__icontains = search ) |
+      Q(student_email__icontains = search ) |
+      Q(student_age__icontains = search )
+      )
+  
+  
+  paginator = Paginator(queryset, 10)
+
+  page_number = request.GET.get("page" , 1)
+  page_obj = paginator.get_page(page_number)
+  
+  print(page_obj)
+  
+  return render(request, 'students.html' , {'queryset' : page_obj})
+  
--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
 from django.urls import path
+from ReportCard.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    
+    path('students/' , get_students , name = "get_students"),
     path("admin/", admin.site.urls),
 ]
+
+
